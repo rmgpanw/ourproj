515083885579bee38c6ea04f189a494240025a32
rmgpanw <rmgpanw@ucl.ac.uk>
2022-11-04 08:48:39 +0000

    add roxygen function documentation for tar_render_workflowr()

--- R/tar_render_workflowr.R
+++ R/tar_render_workflowr.R
@@ -5,6 +5,30 @@
 
 # for guidance, see https://wlandau.github.io/targetopia/contributing.html
 
+#' Target factory for workflowr R markdown reports.
+#'
+#' Generates a list of target objects to render multiple workflowr R markdown
+#' reports.
+#'
+#' @param output_dir Directory where html report files will be rendered to. By
+#'   default this is 'public'.
+#' @param workflowr_dir Directory where workflowr Rmd files are located. By
+#'   default this is 'analysis'.
+#' @param target_prefix Prefix for workflowr target names.
+#' @param include Optional character vector of .Rmd files to be rendered.
+#' @param exclude Optional character vector of .Rmd files to be excluded.
+#' @param include_pattern By default, include all .Rmd files in `workflowr_dir`.
+#' @param exclude_pattern By default, exclude any files with names prefixed by
+#'   "_".
+#' @param verbose If `TRUE`, prints a message listing the .Rmd files for which
+#'   targets will be created.
+#'
+#' @return A list of target objects with `format = 'file'`, one for each
+#'   workflowr report.
+#' @export
+#'
+#' @examples
+#' #TODO
 tar_render_workflowr <- function(output_dir = "public",
                                  workflowr_dir = "analysis",
                                  target_prefix = "WFLWR_",
@@ -67,6 +91,13 @@ tar_render_workflowr <- function(output_dir = "public",
 
 # PRIVATE -----------------------------------------------------------------
 
+#' Target factory for a single workflowr R markdown report
+#'
+#' Helper function for [tar_render_workflowr()].
+#'
+#' @inheritParams tar_render_workflowr
+#'
+#' @return
 tar_render_workflowr_single <- function(rmd_filename,
                                         workflowr_dir = "analysis",
                                         output_dir = "public",
